c2e1daf3098357be2c122fd424c805671ddf4f93
Clément Walter <clement0walter@gmail.com>
2025-12-21 18:47:00 +0200

    cw/design doc 2 (#55)
    * Added `// test comment from cw` to `crates/transpiler/src/vm_exe.rs:13`.
    
    * design doc
    
    * format design doc
    
    * review up to 3.4 (af)
    
    * design doc up to 3.6 (af)
    
    * Add guest scaffolding
    
    * add globals and linker
    
    * added fixed design2.md up to 2.4 (memory model)
    
    * fix workspace and trunk config
    
    * fix design 1, 2, 3, 4
    
    * use main directly so that bin main is just glue code
    
    * fix test lenght too big in linked
    
    * Remove code snippet for guest code
    
    * fix addresses in doc VS linker
    
    * fix trunk
    
    * add setup ci for riscv target
    
    * redo trunk in ci
    
    ---------
    
    Co-authored-by: Antoine FONDEUR <antoine.fondeur@gmail.com>

---
+++ guest/guest-bin/src/main.rs
@@ -0,0 +1,61 @@
+#![no_std]
+#![no_main]
+
+use core::arch::global_asm;
+use core::panic::PanicInfo;
+use guest_lib::main;
+
+// -----------------------------------------------------------------------------
+// Startup assembly (ELF entrypoint)
+// -----------------------------------------------------------------------------
+
+global_asm!(
+    r#"
+    .section .text._start
+    .globl _start
+_start:
+    .option push
+    .option norelax
+    la gp, __global_pointer$
+    .option pop
+
+    la sp, __stack_top
+    lw sp, 0(sp)
+
+    call __zkvm_start
+"#
+);
+
+// -----------------------------------------------------------------------------
+// Global variables
+// -----------------------------------------------------------------------------
+
+static mut INITIALIZED_COUNT: u32 = 42;
+static mut ZERO_PAGE: [u8; 128] = [0; 128];
+
+// -----------------------------------------------------------------------------
+// Rust entry shim
+// -----------------------------------------------------------------------------
+
+#[unsafe(no_mangle)]
+pub extern "C" fn __zkvm_start() -> ! {
+    let value = main();
+    // Put return value in a dedicated memory location
+    unsafe {
+        INITIALIZED_COUNT = INITIALIZED_COUNT.wrapping_add(value);
+        ZERO_PAGE[0] = ZERO_PAGE[0].wrapping_add((INITIALIZED_COUNT & 0xFF) as u8);
+        let sum_with_page = value.wrapping_add(ZERO_PAGE[0] as u32);
+        ZERO_PAGE[1] = ZERO_PAGE[1].wrapping_add(sum_with_page as u8);
+    }
+    /* trunk-ignore(clippy/empty_loop) */
+    loop {}
+}
+
+// -----------------------------------------------------------------------------
+// Panic handler
+// -----------------------------------------------------------------------------
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    loop {}
+}
