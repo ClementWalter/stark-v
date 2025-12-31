d1b4a59d6b1ef44ba3925944d7ae2734a4266f6a
Antoine FONDEUR <antoine.fondeur@gmail.com>
2025-12-31 09:33:14 +0100

    auipc works only for M31 immediates

--- guest/guest-bin/src/bin/auipc.rs
+++ guest/guest-bin/src/bin/auipc.rs
@@ -11,13 +11,6 @@ pub extern "C" fn __zkvm_start() -> ! {
         asm!(
             "auipc t0, 0x00000",
             "auipc t1, 0x00001",
-            "auipc t2, 0x7FFFF",
-            "auipc t3, 0x80000",
-            "auipc t4, 0xFFFFF",
-            "auipc t5, 0x12345",
-            "auipc t6, 0xABCDE",
-            "auipc t0, 0x40000",
-            "auipc t1, 0xC0000",
             options(nostack, nomem)
         );
     }
